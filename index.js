80486ade2ed7ecef758895704770af3cfd95dc69
Andie Samadoulougou <andiesamadoulougou@Andies-MacBook-Pro.local>
2022-07-30 23:11:39 -0400

    add first local storage usage

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+// document.getElementById("quiz").onclick = function () {
+//     location.href = "quiz.html";
+// };
+// document.getElementById("mobile").onclick = function () {
+//     location.href = "mobile.html";
+// };
+// document.getElementById("web").onclick = function () {
+//     location.href = "web.html";
+// };
+// document.getElementById("desktop").onclick = function () {
+//     location.href = "desktop.html";
+// };
+// document.getElementById("linux").onclick = function () {
+//     location.href = "linux.html";
+// };
